34ef4da05192be418b79dad2051d8e69ebeb2f9f
GabriellaXue <fanxue2@illinois.edu>
2021-04-13 17:41:43 -0500

    take input from stdin and ignore ide specific files and folders

--- main.cpp
+++ main.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <tuple>
-#include <string>
-#include <sstream>
+using std::cout;
+using std::endl;
 
-using namespace std;
+#include <fstream>
+using std::ifstream;
+
+#include <string>
+using std::string;
+
+#include <vector>
+using std::vector;
+
+#include <tuple>
+using std::tuple;
+
+#include <sstream>
+using std::stringstream;
+
+#include <queue>
+using std::queue;
+
+#include "simulation.h"
+
 
 int main() {
     const int inputLen = 3;                             // each customer should have three input vars
+    const int MAX_LEN = 256;
 
     int numRegister;                                    // number of registers
-    string tmp;                                         // hold string read from file temporarily
-    vector< tuple<string, int, int> > customerList;        // all the customers in the input file
-    ifstream infile;
+    queue< tuple<string, int, int> > customerList;      // all the customers in the input file
     string delimiter = " ";
+    char line[MAX_LEN - 1];                             // hold line read from input temporarily
 
-    infile.open("input.txt");
-    while(!infile.eof()) {
-        getline(infile, tmp);
+    while(std::cin.getline(line, MAX_LEN)) {
+        string tmp(line);
         if (tmp.length() == 1) {
             stringstream convertNum(tmp);
             convertNum >> numRegister;
@@ -44,14 +58,16 @@ int main() {
             stringstream convertNum2(splittedVal[2]);
             convertNum2 >> third;
             tuple<string, int, int> newTuple = make_tuple(splittedVal[0], second, third);
-            customerList.push_back(newTuple);
+            customerList.push(newTuple);
         }
     }
-    infile.close();
 
     // FOR TESTING USE
     // cout << get<0>(customerList[0]) << " " << get<1>(customerList[0]) << " " << get<2>(customerList[0]) << " " << endl;
     // cout << get<0>(customerList[1]) << " " << get<1>(customerList[1]) << " " << get<2>(customerList[1]) << " " << endl;
     // cout << numRegister << endl;
+
+    Simulation newSim(numRegister, customerList);
+    int holdVal = newSim.simulate();
     return 0;
 }
